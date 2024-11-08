7f89ae59ea7d6bd5621d8a2f327057f42309efb7
fberger <31159436+fberger-xyz@users.noreply.github.com>
2024-11-08 11:50:03 +0100

    Initial commit

---
+++ src/stores/params.store.ts
@@ -0,0 +1,31 @@
+import { SupportedChains } from '@/enums'
+import { create } from 'zustand'
+
+export type ParsedParam = { value: string; isAddress: boolean }
+
+export const useParamsStore = create<{
+    rawSafes: string
+    parsedSafes: { value: string; isAddress: boolean }[]
+    selectedSafe: string
+    rawChain: string
+    selectedChain: SupportedChains
+    actions: {
+        setParams: (rawSafes: string, parsedSafes: ParsedParam[], selectedSafe: string, rawChain: string, selectedChain: SupportedChains) => void
+        setSelectedAddress: (selectedSafe: string) => void
+        setSelectedChain: (selectedChain: SupportedChains) => void
+    }
+    computeds: Record<string, () => void>
+}>((set) => ({
+    rawSafes: '',
+    parsedSafes: [],
+    selectedSafe: '',
+    rawChain: '',
+    selectedChain: SupportedChains.ETH,
+    actions: {
+        setParams: (rawSafes, parsedSafes, selectedSafe, rawChain, selectedChain) =>
+            set(() => ({ rawSafes, parsedSafes, selectedSafe, rawChain, selectedChain })),
+        setSelectedAddress: (selectedSafe) => set(() => ({ selectedSafe })),
+        setSelectedChain: (selectedChain) => set(() => ({ selectedChain })),
+    },
+    computeds: {},
+}))
