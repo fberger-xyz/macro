de064386ae43770d54b76b3aeef4a3573ca066f2
Francis Berger <fberger@coinshares.com>
2024-11-08 18:05:21 +0100

    feat: strange duplicate key non issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,36 +15,34 @@ export default function Page() {
                     <div key={ressourceIndex} className="flex flex-col rounded-md">
                         <p className="text-sm text-primary">{ressource.name}</p>
                         {ressource.proxies.map((proxy, proxyIndex) => (
-                            <div className="w-full" key={`${ressourceIndex}-${proxyIndex}`}>
-                                <LinkWrapper
-                                    target="_blank"
-                                    href={proxy.socials[0].href}
-                                    className="group flex w-full justify-between gap-2 rounded-sm px-1 hover:bg-very-light-hover md:items-center"
-                                >
-                                    <div className="flex flex-wrap items-baseline gap-2 py-1 text-xs md:text-sm">
-                                        <p className="text-inactive">{proxyIndex + 1}.</p>
-                                        {proxy.author && <p>{proxy.author}</p>}
-                                        {proxy.author && proxy.name && <p className="text-inactive">{'>'}</p>}
-                                        {proxy.name && <p className="text-secondary">{proxy.name}</p>}
-                                        {proxy.toMonitor.length > 0 && <p className="text-inactive">{'>'}</p>}
-                                        {proxy.toMonitor.length > 0 && <p className="text-default">{proxy.toMonitor}</p>}
-                                    </div>
-                                    <div className="hidden grow border-b border-dashed border-light-hover text-sm group-hover:border-secondary md:flex" />
-                                    <div className="flex gap-2">
-                                        {proxy.socials.map((social, socialIndex) => (
-                                            <div key={`${ressourceIndex}-${proxyIndex}-${socialIndex}`}>
-                                                <LinkWrapper
-                                                    target="_blank"
-                                                    href={social.href}
-                                                    className="cursor-alias rounded-md p-1 hover:bg-light-hover hover:text-primary"
-                                                >
-                                                    <IconWrapper icon={social.icon} className="size-4" />
-                                                </LinkWrapper>
-                                            </div>
-                                        ))}
-                                    </div>
-                                </LinkWrapper>
-                            </div>
+                            <LinkWrapper
+                                target="_blank"
+                                href={proxy.socials[0].href}
+                                key={`${ressourceIndex}-${proxyIndex}`}
+                                className="group flex w-full justify-between gap-2 rounded-sm px-1 hover:bg-very-light-hover md:items-center"
+                            >
+                                <div className="flex flex-wrap items-baseline gap-2 py-1 text-xs md:text-sm">
+                                    <p className="text-inactive">{proxyIndex + 1}.</p>
+                                    {proxy.author && <p>{proxy.author}</p>}
+                                    {proxy.author && proxy.name && <p className="text-inactive">{'>'}</p>}
+                                    {proxy.name && <p className="text-secondary">{proxy.name}</p>}
+                                    {proxy.toMonitor.length > 0 && <p className="text-inactive">{'>'}</p>}
+                                    {proxy.toMonitor.length > 0 && <p className="text-default">{proxy.toMonitor}</p>}
+                                </div>
+                                <div className="hidden grow border-b border-dashed border-light-hover text-sm group-hover:border-secondary md:flex" />
+                                <div className="flex gap-2">
+                                    {proxy.socials.map((social, socialIndex) => (
+                                        <LinkWrapper
+                                            key={`${ressourceIndex}-${proxyIndex}-${socialIndex}`}
+                                            target="_blank"
+                                            href={social.href}
+                                            className="cursor-alias rounded-md p-1 hover:bg-light-hover hover:text-primary"
+                                        >
+                                            <IconWrapper icon={social.icon} className="size-4" />
+                                        </LinkWrapper>
+                                    ))}
+                                </div>
+                            </LinkWrapper>
                         ))}
                     </div>
                 ))}
